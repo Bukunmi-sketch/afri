6dec057d84d7401924c06ddbadef48a216d027c4
Bukunmi Olarinde <58373089+Bukunmi-sketch@users.noreply.github.com>
2022-07-19 22:00:36 -0700

    --
    --

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,11 +5,15 @@
          <h2 class="logo"> Dashboard </h2>
          
          <div class="create">
-           <a href="#" class="profile">
-             <div class="header-profile-photo">
-             <img src="" alt="">
-             </div>
-           </a>  
+                @auth
+                <a href="{{ url('/logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+      
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
+            @endauth
          </div>
     </div>
 </nav>
