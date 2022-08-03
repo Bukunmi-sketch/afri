feadce850f2676eb912992fd7451c48d126816fa
Bukunmi Olarinde <58373089+Bukunmi-sketch@users.noreply.github.com>
2022-08-03 03:16:12 -0700

    --
    rave composer

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -15,8 +15,51 @@ class paymentController extends Controller
      public function verify(Request $request)
      {
            $transactionid= $request->transaction_id;
+
+           $flw = new \Flutterwave\Rave(getenv('FLW_SECRET_KEY')); // Set `PUBLIC_KEY` as an environment variable
+           $transactions = new \Flutterwave\Transactions();
+           $response = $transactions->verifyTransaction(['id' => $transactionId]);
+           if (
+               $response['data']['status'] === "successful"
+               && $response['data']['amount'] === $expectedAmount
+               && $response['data']['currency'] === $expectedCurrency) {
+               // Success! Confirm the customer's payment
+           } else {
+               // Inform the customer their payment was unsuccessful
+           }
         
         
+     }
+
+     public function confirmpay(){
+        
+      $curl = curl_init();
+
+			curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://api.flutterwave.com/v3/transactions/{$txid}/verify", // Pass transaction ID for validation
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_HTTPHEADER => array(
+			"Content-Type: application/json",
+			'Authorization: Bearer {SECRETE_KEY}', //Get your Secrete key from flutterwave dashboard.
+			),
+			));
+
+			$response = curl_exec($curl);
+
+			curl_close($curl);
+
+			// echo  '<pre>';
+			// echo $response;
+			// echo '</pre>';
+
+			$res = json_decode($response);
+
      }
 
    
